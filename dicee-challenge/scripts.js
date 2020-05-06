79c8e757add1445713c764d070bb599949e3a3e1
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2020-05-06 18:16:05 +0530

    added math.floor instead of math.round

--- dicee-challenge/scripts.js
+++ dicee-challenge/scripts.js
@@ -1,7 +1,7 @@
-var randomNumber1 = Math.round((Math.random() * 5) + 1);
+var randomNumber1 = Math.floor((Math.random() * 5) + 1);
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-var randomNumber2 = Math.round((Math.random() * 5) + 1);
+var randomNumber2 = Math.floor((Math.random() * 5) + 1);
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 === randomNumber2) {
