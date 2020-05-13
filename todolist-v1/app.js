df5806d00abf5047fc81c5306c50dd5e5b6f19ed
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2020-05-13 18:14:04 +0530

    adds layouts

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -51,10 +51,8 @@ app.get("/work", function(req, res) {
   });
 });
 
-app.post("/work", function(req, res) {
-  let item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 
